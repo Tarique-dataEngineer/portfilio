82dc7e6d8e944ebcf0ce0e3d3ec6c0c918da2893
Tarique-dataEngineer <tariqueisrar7834@gmail.com>
2024-03-04 11:40:28 +0530

    profile hosting

---
+++ index.php
@@ -0,0 +1,427 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Mirrored from bdgroup.in/resume/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 12 Jan 2024 09:17:48 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Online CV - Professional HTML CSS Resume Website Template</title>
+
+    <!-- favicon -->
+    <link href="favicon.png" rel=icon>
+
+    <!-- web-fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Hind:300,400,500,600,700" rel="stylesheet">
+
+    <!-- font-awesome -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Style CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar">
+<div id="main-wrapper">
+<!-- Page Preloader -->
+<div id="preloader">
+    <div id="status">
+        <div class="status-mes"></div>
+    </div>
+</div>
+
+<div class="columns-block">
+<div class="left-col-block blocks">
+    <header class="header">
+        <div class="content text-center">
+            <h1>Tarique Israr</h1>
+
+            <p class="lead">Data Analyst</p>
+            <ul class="social-icon">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-slack" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+        <div class="profile-img">
+            <img style="width:100%;" src="img/profile-pic.jpeg" alt="">
+        </div>
+    </header>
+    <!-- .header--> 
+</div>
+
+
+<div class="right-col-block blocks">
+<!-- navgation CSS -->
+<nav class="navbar navbar-default">
+    <!-- mobile button -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Navigasyonu aç/kapa</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <!-- navigation-->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#">HOME</a></li>
+        <li><a href="#exp">EXPERIENCE</a></li>
+        <li><a href="#edu">EDUCATION</a></li>
+        <li><a href="#port">PROJECTS</a></li>
+        <li><a href="#cont">CONTACT</a></li>
+      </ul>
+    </div><!-- navigation end -->
+</nav>	
+
+<section class="intro section-wrapper" id="about">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>about</h2>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <p>Greeting!!!! I’m Tarique, The type of person who loves data and is always excited about new technology and cool innovations. With a creative mind, I enjoy exploring the world of data and the latest tech developments. The experience i have gave me a good grasp of handling data, finding information, using stats, and creating visuals. I also learned about databases. I know about machine learning, deep learning, and natural language processing. <br>
+                </p>
+                <p>With this knowledge, I've done some interesting projects with data, using advanced methods to find important info. I really enjoy dealing with data, and it makes me curious about everything around me. Learning from data and finding patterns is something I'm enthusiastic about. It's like solving a puzzle and understanding more about the world.             <br>   </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section-wrapper section-education gray-bg" id="edu">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title"><h2>Education</h2></div>
+            </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content-item">
+                    <small>2022 - 2023</small>
+                    <h3>Jawaharlal Nehru University</h3>
+                    <h4>Post Graduate Diploma in Big Data Analytics</h4>
+
+                    <p>New Delhi, India</p>
+                </div>
+                <!-- .experience-item -->
+                <div class="content-item">
+                    <small>2017 - 2021</small>
+                    <h3>Tilka Manjhi Bhagalpur University </h3>
+                    <h4>Bachelor of Information Technology</h4>
+
+                    <p>Bhagalpur, Bihar</p>
+                </div>
+            </div>
+        </div>
+        <!--.row-->
+    </div>
+    <!-- .container-fluid -->
+
+</section>
+<!-- .section-education -->
+
+
+
+<section class="section-wrapper section-experience" id="exp">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title"><h2>Work Experience</h2></div>
+            </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content-item">
+                    <small>July 2023 - Sep 2023</small>
+                    <h3>Intern - Big Data and Business Analytics</h3>
+                    <h4>LG Electronics Pvt Ltd, Corporate office, Noida</h4>
+                    
+                   
+                    <h6>Aspect-Based Sentiment Analysis Project using NLP collaborated with LG Electronics</h6>
+                    <p>
+                    
+                        <li>I was in a big data team and responsible for Scraping data of LG products, collecting and analyzing data,
+building an NLP model for dependency parsing and Aspect-Based Sentiment analysis to provide actionable
+insights.
+</li>
+                        
+                    </p>
+                </div>
+
+                <div class="content-item">
+                    <small>Dec 2023</small>
+                    <h3>Data Analysis and Web form Development with IIT Bombay on Covid-19</h3>
+                    
+                    
+                   
+                   <p>
+                    
+                        <li> Perform data analysis on the COVID-19 dataset, examining trends, patterns, and impacts on mortality rates</li>
+                        <li> Build a basic web form on localhost for "Chain for Children's Cheer" to gather volunteer information including
+name, email, phone, address, profession, and interests, with data stored in a PHP SQL database upon
+submission.
+</li>
+                    </p>
+                </div>
+
+                <!-- .experience-item -->
+                <div class="content-item">
+                    
+                    <h3>ACADEMIC PROJECTS <br></h3>
+
+                    <li>I worked on developing a CNN model for brain tumor classification to identify brain tumors from images with
+92% accuracy. Additionally, Collect articles data through Scraping and developed a recommendation system
+using cosine similarity to suggest related medical articles based on titles, achieving a 70-80% accuracy rate
+in testing.
+</li>
+                </div>
+                <!-- .experience-item -->
+                
+                <!-- .experience-item -->
+            </div>
+        </div>
+        <!--.row-->
+    </div>
+    <!-- .container-fluid -->
+
+</section>
+<!-- .section-experience -->
+
+
+
+<!-- addind section for skillls -->
+<section class="section-wrapper skills-wrapper gray-bg">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Skills</h2>
+                </div>
+            </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="progress-wrapper">
+
+                    <div class="progress-item">
+                        <span class="progress-title">Python, SQL </span>
+
+                        
+                        <!-- .progress -->
+                    </div>
+                    <!-- .skill-progress -->
+
+
+                    <div class="progress-item">
+                        <span class="progress-title">Flask Frame Work (Web developments) </span>
+
+                       
+                        <!-- /.progress -->
+                    </div>
+                    <!-- /.skill-progress -->
+
+
+                    <div class="progress-item">
+                        <span class="progress-title">Linux, Window</span>
+
+                        
+                        <!-- /.progress -->
+                    </div>
+                    <!-- /.skill-progress -->
+
+                    <div class="progress-item">
+                        <span class="progress-title">Data Scraping (Selenium, Chrome driver)</span>
+
+                    
+                        <!-- /.progress -->
+                    </div>
+                    <div class="progress-item">
+                        <span class="progress-title">Data Preprocessing and Mining (NumPy, Scikit-Learn, SQL-Queries)</span>
+
+                      
+                        <!-- .progress -->
+                    </div>
+
+                    <!-- /.skill-progress -->
+                    <div class="progress-item">
+                        <span class="progress-title">EDA,Feature Engineering (Pandas, Feature-engine)</span>
+
+                      
+                        <!-- .progress -->
+                    </div>
+                    <div class="progress-item">
+                        <span class="progress-title">Statistics, Machine Learning, Deep Learning, NLP. (Scikit-Learn, Tensorflow, keras)</span>
+
+                      
+                        <!-- .progress -->
+                    </div>
+                    <div class="progress-item">
+                        <span class="progress-title">Excel, Tableau, Visualization (Reports, Dashboards, Matplotlib, Seaborn)</span>
+
+                      
+                        <!-- .progress -->
+                    </div>
+                    <!-- .skill-progress -->
+
+                </div>
+                <!-- /.progress-wrapper -->
+            </div>
+        </div>
+        <!--.row -->
+    </div>
+    <!-- .container-fluid -->
+</section>
+<!-- .skills-wrapper -->
+<!-- New section adding for certifications-->
+<section class="section-wrapper section-experience" id="exp">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title"><h2>Certifications</h2></div>
+            </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content-item">
+                <li>Big Data Hadoop and Spark Training by SimpliLearn.</li>    
+                <li>Career Essentials in Data Analysis by Microsoft</li>
+                <li>Career Essentials in Generative AI by Microsoft</li>
+                <li>Python Data Science by European Open University.</li>
+                <li>Statistics for Data Science by Great Learning.</li>
+                <li>SQL by HackerRank.</li>
+                    
+                </div>
+                <!-- .experience-item -->
+               
+                <!-- .experience-item -->
+            </div>
+        </div>
+        <!--.row-->
+    </div>
+    <!-- .container-fluid -->
+
+</section>
+<!-- interest -->
+
+
+<!-- .section-publications -->
+
+<section class="section-wrapper portfolio-section gray-bg" id="port">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Projects</h2>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-6">
+    <a class="portfolio-item" href="#">
+        <div class="portfolio-thumb">
+            <img src="img/portfolio-5.jpg" alt="">
+        </div>
+
+        <div class="portfolio-info">
+            <h3>Personal HealthCare Assistant</h3>
+            <small>domain.com</small>
+        </div>
+        <!-- portfolio-info -->
+    </a>
+    <!-- .portfolio-item -->
+</div>
+
+<div class="col-md-6">
+    <a class="portfolio-item" href="#">
+        <div class="portfolio-thumb">
+            <img src="img/portfolio-5.jpg" alt="">
+        </div>
+
+        <div class="portfolio-info">
+            <h3>HealthCare Assistant</h3>
+            <small>domain.com</small>
+        </div>
+        <!-- portfolio-info -->
+    </a>
+    <!-- .portfolio-item -->
+</div>
+
+        <!-- /.row -->
+    </div>
+</section>
+<!-- .portfolio -->
+
+<section class="section-contact section-wrapper" id="cont">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Contact</h2>
+                </div>
+            </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+            <div class="col-md-12">
+                <address>
+                    <strong>Address</strong><br>
+                    New Delhi, India
+                </address>
+                <address>
+                    <strong>Phone Number</strong><br>
+                    +9188 0488 5004
+                </address>
+
+                <address>
+                    <strong>Email</strong><br>
+                    <a href="mailto:#">tariqueisrar7834@gmail.com</a>
+                </address>
+            </div>
+        </div>
+        <!--.row-->
+        
+    </div>
+    <!--.container-fluid-->
+</section>
+<!--.section-contact-->
+
+
+<!-- .footer -->
+</div>
+<!-- .right-col-block -->
+</div>
+<!-- .columns-block -->
+</div>
+<!-- #main-wrapper -->
+
+<!-- jquery -->
+<script src="js/jquery-2.1.4.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+
+<!-- Mirrored from bdgroup.in/resume/ by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 12 Jan 2024 09:17:54 GMT -->
+</html>
